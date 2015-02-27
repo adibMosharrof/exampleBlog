53e4fc7c97465b86f1ad7d8949b07b160a5613bf
Adib <adib.mosharrof@gmail.com>
2015-02-27 14:29:31 +0600

    Using PostsRepository with DI in PostsController

--- Blog.Api/Controllers/PostsController.cs
+++ Blog.Api/Controllers/PostsController.cs
@@ -1,50 +1,32 @@
-﻿using Blog.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Threading.Tasks;
-using System.Data.Entity;
-using System.Net.Http;
+﻿using System.Linq;
 using System.Web.Http.OData;
 using Blog.Models.Entities;
 using Blog.Models.Repositories;
 
 namespace Blog.Api.Controllers
 {
-	//[Queryable]
-	//public class PostsController : EntitySetController<PostDto, int>
 	public class PostsController : EntitySetController<Post, int>
-	//public class PostsController : ODataController
 	{
-		PostsRepository postsRepository = new PostsRepository();
+	    private readonly IPostsRepository _postsRepository ;
+
+	    public PostsController(PostsRepository postsPostsRepository)
+	    {
+	        _postsRepository = postsPostsRepository;
+	    }
 		private bool PostExists(int key)
 		{
-			return postsRepository.PostExists(key);
+			return _postsRepository.Exists(key);
 		}
 
 		public override IQueryable<Post> Get()
-		//public override  IQueryable<PostDto> Get()
 		{
-			return postsRepository.Get();
+			return _postsRepository.Get();
 		}
 
 
-		//public SingleResult<OnePostDto> Get([FromODataUri] int key)
-		//{
-		//    IQueryable<OnePostDto> result = postsRepository.Get(key);
-		//    return SingleResult.Create(result);
-		//}
-
-		//public IHttpActionResult Post(Post post)
-		//{
-		//    if (!ModelState.IsValid)
-		//    {
-		//        return BadRequest(ModelState);
-		//    }
-		//    postsRepository.Post(post);
-		//    return Created(post);
-		//}
+	    protected override Post GetEntityByKey(int key)
+	    {
+	        return _postsRepository.Get(key);
+	    }
 	}
 }
