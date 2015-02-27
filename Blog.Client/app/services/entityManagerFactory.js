8f0a5b68e188a8de99639c0906de31a760f142d5
Adib <adib.mosharrof@gmail.com>
2015-02-27 14:34:13 +0600

    Separated breeze configuration into entityManagerFactory
    Added entitymanager factory to index.html page

--- Blog.Client/app/services/entityManagerFactory.js
+++ Blog.Client/app/services/entityManagerFactory.js
@@ -5,20 +5,27 @@
          .factory('entityManagerFactory', ['breeze', emFactory]);
 
     function emFactory(breeze) {
-        // Convert properties between server-side PascalCase and client-side camelCase
-        breeze.NamingConvention.camelCase.setAsDefault();
-        breeze.config.initializeAdapterInstance('dataService', 'webApiOData', true);
-        // Identify the endpoint for the remote data service
-        var serviceRoot = window.location.protocol + '//' + window.location.host + '/';
-        //var serviceName = serviceRoot + 'breeze/breeze'; // breeze Web API controller
-        var serviceName ="localhost/BlogApi/"; // breeze Web API controller
-
-        // the "factory" services exposes two members
+        configureBreeze();
+        var serviceName = "http://localhost/BlogApi";
         var factory = {
-            newManager: function () { return new breeze.EntityManager(serviceName); },
+            newManager: newManager,
             serviceName: serviceName
         };
 
         return factory;
+
+        function configureBreeze() {
+            // use Web API OData to query and save
+            breeze.config.initializeAdapterInstance('dataService', 'webApiOData', true);
+
+            // convert between server-side PascalCase and client-side camelCase
+            breeze.NamingConvention.camelCase.setAsDefault();
+        }
+
+        function newManager() {
+            var mgr = new breeze.EntityManager(serviceName);
+            return mgr;
+        }
     }
+
 })();
