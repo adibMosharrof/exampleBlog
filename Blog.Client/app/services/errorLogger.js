68c5c73da4819bfb53ddb1449ca22f307dd06332
Adib <adib.mosharrof@gmail.com>
2015-02-27 14:35:44 +0600

    Added error logger factory

---
+++ Blog.Client/app/services/errorLogger.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('blog')
+        .factory('errorLogger', errorLogger);
+
+    errorLogger.$inject = [];
+
+    function errorLogger() {
+        var service = {
+            logError: logError
+        };
+
+        return service;
+
+        function logError(error) {
+            //logger.error(error.message, "Query failed");
+            alert('query failed');
+            return $q.reject(error); // so downstream promise users know it failed
+        }
+    }
+})();
