b746091a26e2e1944475a5bd9252038fe10c9f44
Adib <adib.mosharrof@gmail.com>
2015-02-27 14:38:34 +0600

    Using entitymanagerfactory and error logger in PostService.js

--- Blog.Client/app/services/PostService.js
+++ Blog.Client/app/services/PostService.js
@@ -5,36 +5,25 @@
         .module('blog')
         .factory('PostService', PostService);
 
-    PostService.$inject = ['breeze', '$q'];
+    PostService.$inject = ['breeze', '$q', 'errorLogger', 'entityManagerFactory'];
 
-    function PostService(breeze, $q) {
-        //this.$http = $http;
-        var _this = this;
+    function PostService(breeze, $q, errorLogger, emFactory) {
         var service = {
             getData: getData,
             getPosts: getPosts
         };
 
+        var manager = emFactory.newManager(); 
         return service;
 
         function getData() { }
 
         function getPosts() {
-            //return _this.$http.get('http://localhost:5326/Posts?$orderby=Id desc');
-            //return _this.$http.get('http://localhost/BlogApi/Posts');
-            //debugger;
-            var dataService = new breeze.DataService({
-                serviceName: 'http://localhost/BlogApi/'
-                //serviceName: 'http://localhost/BlogApi/',
-                //hasServerMetadata: false
-            });
-            breeze.NamingConvention.camelCase.setAsDefault();
+            var query = breeze.EntityQuery.from("Posts").take(5);
+            var promise = manager.executeQuery(query).catch(errorLogger.logError);
+            return promise;
+        }
 
-            breeze.config.initializeAdapterInstance('dataService', 'webApiOData', true);
-            var manager = new breeze.EntityManager({ dataService :dataService});
-            var query = breeze.EntityQuery.from("Posts").take(5).expand("comments");
-            //var query = breeze.EntityQuery.from("Posts").take(5);
-            var promise = manager.executeQuery(query).catch(queryFailed);
             return promise;
     }
 
