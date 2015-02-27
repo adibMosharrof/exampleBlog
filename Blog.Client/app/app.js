bda3a086805e5c4cb50435f4abd320c4e9fb2f1e
Adib <adib.mosharrof@gmail.com>
2015-02-27 14:58:12 +0600

    Moved routing configurations from app.js to routeStates.js

--- Blog.Client/app/app.js
+++ Blog.Client/app/app.js
@@ -5,17 +5,6 @@ var app = angular.module('blog', [
     'ngGrid'
 ]);
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-    var viewBaseUrl = "/app/views/";
-    $urlRouterProvider.otherwise('/home');
-
-    $stateProvider.state('home', {
-        url: '/home',
-        templateUrl: viewBaseUrl + 'home.html',
-        controller : 'PostCtrl'
-    }).state('about', {});
-});
-
 // Execute bootstrapping code and any dependencies.
 app.run([
     '$q', '$rootScope', 'breeze',function ($q, $rootScope, breeze) {
---
+++ Blog.Client/app/configurations/routeStates.js
@@ -0,0 +1,16 @@
+﻿app.config(function ($stateProvider, $urlRouterProvider) {
+    var viewBaseUrl = "/app/views/";
+    $urlRouterProvider.otherwise('/posts');
+
+    $stateProvider
+        .state('posts', {
+            url: '/posts',
+            templateUrl: viewBaseUrl + 'posts/home.html',
+            controller: 'PostCtrl'
+        })
+        .state('addPost', {
+            url: '/posts/create',
+            templateUrl: viewBaseUrl + 'posts/create.html',
+            controller : 'PostCtrl'
+        });
+});
